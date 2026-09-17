e54b866ed7e2bad3a27f69d20f867e4d58bd4851
Rahul Kumar Singh <rahulkumarsingh42025@gmail.com>
2026-09-17 13:04:44 +0530

    Configure Firebase for MindQuest authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'models/profile.dart';
 import 'models/puzzle.dart';
 import 'services/profile_store.dart';
@@ -8,6 +9,7 @@ import 'services/puzzle_engine.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await MobileAds.instance.initialize();
+  await Firebase.initializeApp();
   final store = ProfileStore();
   await store.load();
   runApp(MindQuestApp(store: store));
